763bb3f094fc24adb99f8d7148e575ad373093f5
akinbolaji10 <bolaji9494@gmail.com>
2025-06-05 23:14:50 +0100

    Update webhook_server.py

--- webhook_server.py
+++ webhook_server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from datetime import datetime
 from dotenv import load_dotenv
 import os
@@ -18,6 +19,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///local.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+CORS(app, origins=["https://maxeng-wallet-signing.onrender.com"])  # Allow requests from sign.html
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
